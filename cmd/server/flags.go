f9c8c1df073f040aba1f0f9d2d175cea4d1b2fca
desktop <dirol481@gmail.com>
2024-12-11 18:17:57 +0300

    Add logging using zap

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -6,12 +6,17 @@ import (
 )
 
 var flagRunAddr string
+var flagLogLevel string
 
 func parseFlags() {
 	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "endpoint address")
+	flag.StringVar(&flagLogLevel, "l", "info", "log level")
 	flag.Parse()
 
 	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
 		flagRunAddr = envRunAddr
+	}
+	if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
+		flagLogLevel = envLogLevel
 	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"metrics-service/internal/server/handler"
+	"metrics-service/internal/server/middleware"
 	"metrics-service/internal/server/storage"
 )
 
@@ -11,6 +12,13 @@ func main() {
 
 	// Обрабатываем аргументы командной строки
 	parseFlags()
+
+	// Создаем логгер
+	mid := middleware.NewMiddleware()
+	err := mid.Initialize(flagLogLevel)
+	if err != nil {
+		return
+	}
 
 	server := gin.Default()
 
@@ -24,13 +32,17 @@ func main() {
 	htmlHandler := handler.NewHTMLHandler(metricsStorage)
 
 	// Роутинг
+
+	// Для всех эндпоинтов используем логирование
+	server.Use(mid.WithLogging())
+
 	server.POST("/update/:metricType/:metricName/:metricVal", metricsHandler.Update)
 
 	server.GET("/value/:metricType/:metricName", metricsHandler.Get)
 
 	server.GET("/", htmlHandler.Get)
 
-	err := server.Run(flagRunAddr)
+	err = server.Run(flagRunAddr)
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
